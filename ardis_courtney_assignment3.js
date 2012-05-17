698af6957d8dd2f52f1269f414e9d4cde5e600e8
Courtney Ardis <ardiscb@fullsail.edu>
2012-05-17 02:52:09 -0400

    Added string function; needs tweaking. Added json data: number, string, and object properties.

--- ardis_courtney_assignment3.js
+++ ardis_courtney_assignment3.js
@@ -9,6 +9,8 @@ Assignment: Deliverable 3
 //Start//
 /////////
 
+//Global variables
+
 var person = function(name, dept, age){
 	var name = "My name is " + name;
 	var dept = "I work in the " + dept + " department.";
@@ -18,8 +20,8 @@ var person = function(name, dept, age){
 		return age;
 	};
 	//Method: Procedure
-	this.something = function(){
-		console.log("Something");
+	this.tickets = function(){
+		console.log("");
 	};
 	//Method: Accessor	
 	this.getName = function(){
@@ -33,59 +35,82 @@ var person = function(name, dept, age){
 		dept = newDept;
 	};
 };
-
 /*
 //Procedure that ...
 var procedure = function(){
 	console.log("");
 };
-
+*/
+/*
 //Number function that tests ... with number an array arguments
-var numFunction = function(numTickets, employee){
+var numFunction = function(hardwareTeam, number){
 	//For loop
-	for(){
+	for (var name in hardwareTeam){
+		//Local variable
+		var person = console.log(name + " is working");
+		//Array return
+		return person;
 		//Nested loop
-		for(){
-	
+		for(n = 0; n > possibleTickets.length; n++){
+			//Number return
+			return number;
 		};
 	};
 };
-
+/*
 //Boolean function that tests ... with boolean arguments
-var boolFunction = function(this, that){
+var boolFunction = function(boolean){
 	//While loop
 	while(){
 		//Math
 		Math.floor(this + that);
-	};
-};
 
-//String function that tests
-var stringFunction = function(string){
-	//Conditional
-	if(){
-		//Nested Conditional
-		if(){
-	
-		};
-	} else {
-	
 	};
-	//Return string
-	return string;
+	//Boolean return
+	return boolean;
 };
 */
+//String function that tests whether the user has a problem and initiates the first question.
+var stringFunction = function(problem, ticket){
+	//Local variable
+	//Conditional
+	if(problem === "no"){
+		prompt("Are you sure you don't have a problem? Answer \"yes\" or \"no \".");
+		return problem;
+	};
+	if(problem === "yes"){
+		console.log("We will get to your problem in the order it has been received.");
+		//Nested Conditional
+		if(ticket = 0){
+			ticket = possibleTickets.tickets.question[i];
+			//Object return
+			return ticket;
+		};
+	} else {
+		var message = "Do you have a problem? Answer \"yes\" or \"no \".";
+		//String return
+		return message;
 
-//console.log this.something (procedure)
-var me = new person("Courtney", "USS", 21);
-console.log(me.getName() + " " + me.getDept() + " " + me.age());
-//set Department
+	};
+	
+};
 
 /*
-Returned Values
-console.log(name);
+//Returned Values
+console.log(message);
 console.log(dept);
 console.log(age);
 */
 
+//Outputs
+//console.log this.something (procedure)
+var me = new person("Courtney", "USS", 21);
+console.log(me.getName() + " " + me.getDept() + " " + me.age());
+//numFunction(9, 1);
+stringFunction("yes", 0);
+//set Department
+
+//////////
+//Finish//
+//////////
 
--- json.js
+++ json.js
@@ -1,21 +1,40 @@
-/* Example json
-var family = {
-	"characters": [
+var hardwareTeam = {
+	"member": [
 		{
-			"name": "Clark Kent",
-			"occupation": "Superman",
-			"age": "20"
+			"name": "Ron",
+			"occupation": "Tier 3",
+			"age": "30"
 		},
 		{
-			"name": "Jonathan Kent",
-			"occupation": "a farmer",
+			"name": "Larry",
+			"occupation": "Tier 2",
 			"age": "46"
 		},
 		{
-			"name": "Martha Kent",
-			"occupation": "a mother",
+			"name": "Mike",
+			"occupation": "Tier 2",
+			"age": "35"
+		},		
+		{
+			"name": "Howie",
+			"occupation": "Tier 2",
 			"age": "42"
 		}
 	]
 };
-*/
+var possibleTickets = {
+	"tickets": [
+		{
+			"issue": "Monitor black",
+			"question": "Cables plugged in?"
+		},
+		{
+			"issue": "Printer not working",
+			"question": "What is the error message?"
+		},
+		{
+			"issue":"Internet down",
+			"question": "Are you directed connected or using wireless?"
+		}
+	]
+};
